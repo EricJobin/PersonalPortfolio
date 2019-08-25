7f11823931d569e9d9e315db036792f7cc603b58
Eric Jobin <ejobin@gmail.com>
2019-08-25 17:20:33 -0400

    Added portfolio cards

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,32 @@
+var anyClick=false;
+
 function displayBio(){
 	$("#about").on("click", function(){
-		$("#aboutMeCardContainer").fadeIn(1000);
+		if(anyClick==false){
+			$("#aboutMeCardContainer").fadeIn(1000);
+			anyClick=true;
+		}
+		else{
+			$("#portfolioContainer").fadeOut(1000);
+			setTimeout(function(){ $("#aboutMeCardContainer").fadeIn(1000); }, 1001);
+		}
+	})
+}
+
+function displayPortfolio(){
+	$("#portfolio").on("click", function(){
+		if(anyClick==false){
+			$("#portfolioContainer").fadeIn(1000);
+			anyClick=true;
+		}
+		else{
+			$("#aboutMeCardContainer").fadeOut(1000);
+			setTimeout(function(){ $("#portfolioContainer").fadeIn(1000); }, 1001);
+		}
 	})
 }
 
 $(document).ready(function() {
 	displayBio();
+	displayPortfolio();
 });
